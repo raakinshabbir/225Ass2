278670e7e40e516f8b4122c22ab60d8da3b4bb59
raak <152353046+raakinshabbir@users.noreply.github.com>
2024-02-12 16:51:29 -0800

    SHSL implementation (need to comment)

--- Q1/Stack.h
+++ Q1/Stack.h
@@ -1,100 +1,90 @@
-//LOL WE FUCKED IT NO TAIL POINTER JUST HEAD POINTER LOLLLLLLLLLLLL
-//way we have it rn is a DHDL
-//need SHSL
-
-
 class Stack {
-private:
+  private:
     // Nodes for a singly-linked list
     class StackNode {
-    public:
-        int data;
-        StackNode *next;
-        
-        // Constructor
-        StackNode(int n) : data(n), next(nullptr) {}
+      public: int data;
+      StackNode * next;
+
+      // Constructor
+      StackNode(int n): data(n),
+      next(nullptr) {}
     };
 
-    StackNode *head;
-    StackNode *tail;
+  StackNode * head;
 
-public:
+  public:
     // Constructor to initialize the stack
-    Stack() : head(nullptr), tail(nullptr) {}
+    Stack(): head(nullptr) {}
 
     // Destructor to clean up memory
     ~Stack() {
-        while (head != nullptr) {
-            StackNode *temp = head;
-            head = head->next;
-            delete temp;
-        }
+      while (head != nullptr) {
+        StackNode * temp = head;
+        head = head -> next;
+        delete temp;
+      }
     }
 
-    // Check if the stack is empty
-    bool isEmpty() {
-        return head == nullptr;
-    }
+  // Check if the stack is empty
+  bool isEmpty() {
+    return head == nullptr;
+  }
 
-    // Push operation to insert element at the top of the stack
-    bool push(int data) {
-        StackNode *new_node = new StackNode(data);
-        if (head == nullptr) {
-            head = new_node;
-            tail = new_node;
-        } else {
-            tail->next = new_node;
-            tail = new_node;
-        }
-        return true;  
+  // Push operation to insert element at the top of the stack
+  bool push(int data) {
+    StackNode * new_node = new StackNode(data);
+    StackNode * current = head;
+    //if the stack is empty 
+    if (head == nullptr) {
+      head = new_node;
     }
-
-    bool pop() {
-    int element;
-    if (head != nullptr) {
-        // If there's only one node in the list
-        if (head == tail) {
-            StackNode *temp = head;
-            element = temp->data;
-            head = nullptr;
-            tail = nullptr;
-            delete temp;
-        } 
-        else {
-            // If there are more than one node in the list
-            StackNode *current = head;
-            while (current->next != tail) {
-                current = current->next;
-            }
-            StackNode *temp = tail;
-            element = temp->data;
-            tail = current;
-            tail->next = nullptr;
-            delete temp;
-        }
-    } 
-    else{
-        return false; 
+    //stack is not empty, traverse the LL and insert the new node at the last element;
+    else {
+      while (current -> next != nullptr) {
+        current = current -> next;
+      }
+      current -> next = new_node;
     }
     return true; 
+  }
+
+bool pop() {
+  if (head == nullptr) {
+    return false;
+  } else if (head -> next == nullptr) {
+    delete head;
+    head = nullptr;
+  } else {
+    StackNode * current = head;
+    while (current -> next -> next != nullptr) {
+      current = current -> next;
+    }
+    delete current -> next;
+    current -> next = nullptr;
+  }
+  return true;
 }
 
-    int peek() {
-        if(head == nullptr){
-            return -1;
-        }
-        else{
-        int element = tail -> data; 
-        return element;
-        } 
+int peek() {
+  int element;
+  StackNode * current = head;
+  if (head == nullptr) {
+    return 999;
+  } else {
+    while (current -> next != nullptr) {
+      current = current -> next;
     }
+    element = current -> data;
+  }
+  return element;
+}
 //TRAVERSE ISNT REAL
-    void traverse() {
-        StackNode *current = head;
-        while (current != nullptr) {
-            std::cout << current->data << " -> ";
-            current = current->next;
-        }
-        std::cout << "nullptr" << std::endl;
-    }
+void traverse() {
+  StackNode * current = head;
+  while (current != nullptr) {
+    std::cout << current -> data << " -> ";
+    current = current -> next;
+  }
+  std::cout << "nullptr" << std::endl;
+}
 };
